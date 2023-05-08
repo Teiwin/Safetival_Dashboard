f6374eed46c3be733e2484cd789ccd130a214864
Rhadamanthe76 <justin.brugiere@gmail.com>
2023-05-08 16:43:34 +0200

    full use of templates and jinja

--- static/js/festival.js
+++ static/js/festival.js
@@ -1,20 +1,72 @@
-const map = L.map("map").fitBounds([
-  [{{ min_lat }}, {{ min_lng }}],
-  [{{ max_lat }}, {{ max_lng }}]
-]);
+// constants defined in the template
+// const map = L.map("map")
+// const participant_positions = [];
+// const startTimestamp = 0;
+// const endTimestamp = 1;
+
+// ------------------ MAP ------------------
+
+const markersGroup = L.layerGroup().addTo(map);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // Add participant positions to the map
-const participant_positions = {{ participant_positions|safe }};
-console.log(participant_positions);
-participant_positions.forEach(([lat, lng]) => {
-    // Use circleMarker with red color and custom radius
-    L.circleMarker([lat, lng], {
-      color: 'red',
-      fillColor: 'red',
-      fillOpacity: 1,
-      radius: 5
-    }).addTo(map);
+function showAllPositions() {
+  participant_positions.forEach(([timestamp, lat, lng]) => {
+      // Use circleMarker with red color and custom radius
+      const marker = L.circleMarker([lat, lng], {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 1,
+        radius: 5
+      })
+      markersGroup.addLayer(marker);
+    });
+}
+showAllPositions()
+
+// ------------------ TIME SLIDER ------------------
+
+const timeSlider = document.getElementById("time-slider");
+const timeSliderValue = document.getElementById("time-slider-value");
+const timeSliderSettings = document.getElementById("time-slider-settings");
+const toggleTimeSliderButton = document.getElementById("toggle-time-slider");
+
+const windowSize = 1 * 60 * 1000; // 10 minutes window in milliseconds
+
+function updateTimestamp() {
+  const sliderValue = parseInt(timeSlider.value);
+  const timestamp = startTimestamp + (sliderValue / 1000) * (endTimestamp - startTimestamp) - windowSize;
+  timeSliderValue.textContent = sliderValue / 10;
+  displayPositionsInWindow(timestamp, timestamp + windowSize);
+}
+
+function displayPositionsInWindow(windowStart, windowEnd) {
+  // Clear previous markers
+  markersGroup.clearLayers();
+
+  // Add markers for positions within the time window
+  participant_positions.forEach(([timestamp, lat, lng]) => {
+    if (timestamp >= windowStart && timestamp <= windowEnd) {
+      const marker = L.circleMarker([lat, lng], {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 1,
+        radius: 5
+      })
+      markersGroup.addLayer(marker);
+    }
   });
+}
+
+timeSlider.addEventListener("input", updateTimestamp);
+
+toggleTimeSliderButton.addEventListener("click", () => {
+  if (timeSliderSettings.style.display === "none") {
+    timeSliderSettings.style.display = "block";
+  } else {
+    timeSliderSettings.style.display = "none";
+    showAllPositions();
+  }
+});
