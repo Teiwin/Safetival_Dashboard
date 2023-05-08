cf4778476b99c46775d4464ec7b21d60b27fc7de
Rhadamanthe76 <justin.brugiere@gmail.com>
2023-05-08 16:42:47 +0200

    get position is also based on event time

--- festival/views.py
+++ festival/views.py
@@ -21,9 +21,9 @@ def festival(request, event_id):
         querry = ("SELECT gps_north, gps_west, sample_date FROM position "
                    "JOIN participation ON position.p_user_id = participation.p_user_id "
                    "WHERE participation.place_id = %s "
-                   "AND gps_north > %s AND gps_north < %s AND gps_west > %s AND gps_west < %s "
+                   "AND gps_north > %s AND gps_north < %s AND gps_west > %s AND gps_west < %s AND sample_date > %s AND sample_date < %s"
         )
-        data = (event_id, min_lat, max_lat, min_lng, max_lng)
+        data = (event_id, min_lat, max_lat, min_lng, max_lng, start_date, end_date)
         cursor.execute(querry, data)
     
         participant_position = []
@@ -35,22 +35,12 @@ def festival(request, event_id):
         querry = ("SELECT COUNT(DISTINCT position.p_user_id) FROM position "
                    "JOIN participation ON position.p_user_id = participation.p_user_id "
                    "WHERE participation.place_id = %s "
-                   "AND gps_north > %s AND gps_north < %s AND gps_west > %s AND gps_west < %s "
+                   "AND gps_north > %s AND gps_north < %s AND gps_west > %s AND gps_west < %s AND sample_date > %s AND sample_date < %s"
         )
-        data = (event_id, min_lat, max_lat, min_lng, max_lng)
+        data = (event_id, min_lat, max_lat, min_lng, max_lng, start_date, end_date)
         cursor.execute(querry, data)
         number_of_participants = cursor.fetchone()[0]
         
-    # fetch the start time and end time of the participations (for the timeline, development)
-    # DEVLOPPEMENT ONLY:!!
-    with connections["postgresql"].cursor() as cursor:
-        querry = ("SELECT min(sample_date), max(sample_date) FROM position "
-                   "JOIN participation ON position.p_user_id = participation.p_user_id "
-                   "WHERE participation.place_id = %s "
-                   "AND gps_north > %s AND gps_north < %s AND gps_west > %s AND gps_west < %s ")
-        data = (event_id, min_lat, max_lat, min_lng, max_lng)
-        cursor.execute(querry, data)
-        start_time, end_time = [i.timestamp() for i in cursor.fetchone()]
 
     context = {
         "event_id": event_id,
@@ -60,8 +50,8 @@ def festival(request, event_id):
         "max_lng": max_lng,
         "participant_positions": participant_position,
         "number_of_participants": number_of_participants,
-        "start_time": start_time,
-        "end_time": end_time,
+        "start_time": start_date.timestamp(),
+        "end_time": end_date.timestamp(),
     }
 
     return render(request, "festival/festival.html", context)
